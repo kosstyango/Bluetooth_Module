26ef850991cae1c27aae47f2a0fb7b95ccb4fde2
Konstantin Barnaev <konstantin.barnaev@gmail.com>
2023-01-05 18:07:13 +0200

    Bluetooth searching module with BTH activation

--- bluetooth/src/main/java/com/example/bluetooth/DeviceListFragment.kt
+++ bluetooth/src/main/java/com/example/bluetooth/DeviceListFragment.kt
@@ -1,22 +1,24 @@
 package com.example.bluetooth
-
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
-import android.graphics.Color
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.NotificationCompat.getColor
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.example.bluetooth.databinding.FragmentListBinding
+import com.google.android.material.snackbar.Snackbar
 
 class DeviceListFragment : Fragment() {
     private var bAdapter: BluetoothAdapter? = null
     private lateinit var binding: FragmentListBinding
-
+    private lateinit var btLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +30,9 @@ class DeviceListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.imageBTH.setOnClickListener{ //Вешаем обработчик на кнопку "Bluetooth"
+         btLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))} //Запрашиваем разрешение у Системы на включение Bluetooth
+        registerBtLauncher()
         initBtAdapter()
         blueToothState()
     }
@@ -37,7 +42,25 @@ class DeviceListFragment : Fragment() {
     bAdapter = bManager.adapter
     }
 
-    private fun blueToothState(){
-        if (bAdapter?.isEnabled == true) {changeButtonColor(binding.imageBTH, ContextCompat.getColor(activity as Context, R.color.DarkGreen))}
+    private fun blueToothState(){ //Проверка состояния Bluetooth
+        if (bAdapter?.isEnabled == true) {
+            changeButtonColor(binding.imageBTH, ContextCompat.getColor(activity as Context, R.color.DarkGreen))
+        }else{
+            changeButtonColor(binding.imageBTH, ContextCompat.getColor(activity as Context, R.color.Red))
         }
+    }
+
+    private fun registerBtLauncher(){ //обработка нажатия на кнопку "Bluetooth"
+        btLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            if (it.resultCode == Activity.RESULT_OK){
+                Snackbar.make(binding.root, "Bluetooth IS activated", Snackbar.LENGTH_LONG).show()
+                changeButtonColor(binding.imageBTH, ContextCompat.getColor(activity as Context, R.color.DarkGreen))
+            }else{
+                changeButtonColor(binding.imageBTH, ContextCompat.getColor(activity as Context, R.color.Red))
+                Snackbar.make(binding.root, "Bluetooth is NOT activated", Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
 }
